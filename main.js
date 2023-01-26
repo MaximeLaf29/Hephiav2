4b07491a61c7a505f071583658b17bc255e90930
lolmaxz <34990860+lolmaxz@users.noreply.github.com>
2023-01-26 14:01:53 -0500

    minor fix on main.js

--- main.js
+++ main.js
@@ -42,7 +42,6 @@ loadEvents(client)
 // We load all the guild's configs into the client object for easy access
 const { loadGuildConfigs } = require('./Functions/configLoader.js')
 loadGuildConfigs(client)
-console.log(__dirname)
 
 // # Config stuff removed from here so it's more clean
 // --------- TEST ZONE -----------//
